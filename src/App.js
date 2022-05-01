213f6ab2751bd094d4adbcdcbb4bd8408d6e6718
MasonM <Mason.P.Marinko@gmail.com>
2022-05-01 12:18:59 -0600

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import './App.css';
+const axios = require('axios').default;
 
-function App() {
+
+const App = () => {
+  const [resData, setResData] = useState([])
+
+  useEffect(() => {
+    let isMounted = true;
+    axios.get(`https://mlb22.theshow.com/apis/listings.json`).then(res => {
+      if (!isMounted) return;
+      setResData(res.data.listings)
+    });
+    return () => isMounted = false;
+  }, []);
+
+  console.log(resData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    resData?.map((r) =>
+      <div className='flex-container'>
+        <img className="one" src={r?.item.img}></img>
+        <div className='two'>
+          <p>
+            Name: {r?.listing_name}
+          </p>
+          <p>
+            Best Sell Price: {r?.best_sell_price}
+          </p>
+          <p>
+            Best Buy Price: {r?.best_buy_price}
+          </p>
+        </div>
+      </div>
+    )
   );
 }
 
