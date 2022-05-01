c6e0d00fc5ba83b61e2be89b11d7b150bc6ece92
MasonM <Mason.P.Marinko@gmail.com>
2022-05-01 16:39:21 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import './App.css';
 const axios = require('axios').default;
 
-
 const App = () => {
   const [resData, setResData] = useState([])
+
+  const testing = (buyPrice, sellPrice) => {
+    const commissionSellPrice = sellPrice - (sellPrice * .10)
+    const buySellDifference = commissionSellPrice - buyPrice
+    if (Math.sign(buySellDifference) === -1) {
+      return ("Losing: " + '$' + Math.abs(buySellDifference.toFixed(2)))
+    } else {
+      return ("Making: " + '$' + buySellDifference.toFixed(2))
+    }
+
+  }
 
   useEffect(() => {
     let isMounted = true;
@@ -21,19 +31,20 @@ const App = () => {
   console.log(resData)
 
   return (
-    resData?.map((r) =>
-      <div className='flex-container'>
-        <img className="one" src={r?.item.img}></img>
-        <div className='two'>
+    resData?.map((r,i) =>
+      <div className='flex-container' key={i}>
+        <img className="card-image" src={r?.item.img}></img>
+        <div className='card-info'>
           <p>
-            Name: {r?.listing_name}
+            Name: {r.listing_name}
           </p>
           <p>
-            Best Sell Price: {r?.best_sell_price}
+            Best Sell Price: ${r.best_sell_price}
           </p>
           <p>
-            Best Buy Price: {r?.best_buy_price}
+            Best Buy Price: ${r.best_buy_price}
           </p>
+          <p>{testing(r.best_buy_price, r.best_sell_price)}</p>
         </div>
       </div>
     )
